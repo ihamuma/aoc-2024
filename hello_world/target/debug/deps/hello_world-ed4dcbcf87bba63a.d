c12debabd013255efcf16e10a3b4b14fad7592ed
ihamuma <markus.ihamuotila@gmail.com>
2024-12-01 22:39:37 +0200

    Rust initialisation

---
+++ hello_world/target/debug/deps/hello_world-ed4dcbcf87bba63a.d
@@ -0,0 +1,5 @@
+/Users/markus/GitHub/aoc-2024/hello_world/target/debug/deps/libhello_world-ed4dcbcf87bba63a.rmeta: src/main.rs
+
+/Users/markus/GitHub/aoc-2024/hello_world/target/debug/deps/hello_world-ed4dcbcf87bba63a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/markus/GitHub/aoc-2024/hello_world/target/debug/hello_world: /Users/markus/GitHub/aoc-2024/hello_world/src/main.rs
