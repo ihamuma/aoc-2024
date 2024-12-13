b34d4ac90e46fafd0b7c7d68211cb6c0f283d26f
ihamuma <markus.ihamuotila@gmail.com>
2024-12-13 16:37:51 +0200

    Day Four Part One Rework

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -5,40 +5,16 @@ fn main() {
     let path: &str = "./input.txt";
     let by_line: Vec<String> = file_to_string_vec(path);
 
-    let horizontal_matches = find_xmas_in_rows(&by_line);
-    println!("Horizontal matches: {}", horizontal_matches);
-
     let mut matrix: Vec<Vec<char>> = Vec::new();
     for line in by_line {
         let as_char_vecs: Vec<char> = line.chars().collect();
         matrix.push(as_char_vecs);
     }
-    
-    let matrix_clone = matrix.clone();
-    let matrix_re_clone = matrix.clone();
-    let transposed = transpose(matrix);
-    let mut trans_clone = transposed.clone();
-    let string_vec = char_to_string_vec(transposed);
-    let vertical_matches = find_xmas_in_rows(&string_vec);
-    println!("Vertical matches: {}", vertical_matches);
 
-    let diagonals_1 = diagonal_matrix(matrix_clone);    
-    let string_vec = char_to_string_vec(diagonals_1);
-    let diagonal_1_matches = find_xmas_in_rows(&string_vec);
-    println!("Diagonal 1 matches: {}", diagonal_1_matches);
+    let xmases = count_xmases(&matrix);
+    println!("The amount of xmases is {}", xmases);
 
-    trans_clone.iter_mut().for_each(|arr| arr.reverse());    
-    let diagonals_2 = diagonal_matrix(trans_clone);    
-    let string_vec = char_to_string_vec(diagonals_2);
-    let diagonal_2_matches = find_xmas_in_rows(&string_vec);
-    println!("Diagonal 2 matches: {}", diagonal_2_matches);
-
-    let total = horizontal_matches + vertical_matches + diagonal_1_matches + diagonal_2_matches;
-
-    println!("Total matches: {}", total);
-
-    let xed_mases = count_xed_mases(&matrix_re_clone);
-
+    let xed_mases = count_xed_mases(&matrix);
     println!("The amount of X-MASes is {}", xed_mases)
 }
 
@@ -57,72 +33,58 @@ fn file_to_string_vec(path: &str) -> Vec<String> {
     by_line
 }
 
-fn find_xmas_in_rows (allwords: &Vec<String>) -> i32{
-    let mut xmascount = 0;
-    let keyword = "XMAS".to_string();
-    let reverse = keyword.chars().rev().collect::<String>();
-    for rows in allwords {
-        let mut i = 0;
-        let mut j = keyword.len();
-        while j <= rows.len() {
-            if rows[i..j] == keyword || rows[i..j] == reverse {
-                xmascount += 1;
+fn count_xmases (matrix: &Vec<Vec<char>>) -> u32 {
+    let mut xmas_count: u32 = 0;
+    let xmas = String::from("XMAS");
+
+    // TODO: Fix this absolutely horrid solution
+    let e_m = expand_matrix(&matrix);
+
+    for i in 3..e_m.len()-3 {
+        for j in 3..e_m[0].len()-3 {
+            if e_m[i][j] == 'X' {
+                let left =  vec!['X', e_m[i][j-1], e_m[i][j-2], e_m[i][j-3]];
+                let right = vec!['X', e_m[i][j+1], e_m[i][j+2], e_m[i][j+3]];
+                let up =    vec!['X', e_m[i-1][j], e_m[i-2][j], e_m[i-3][j]];
+                let down =  vec!['X', e_m[i+1][j], e_m[i+2][j], e_m[i+3][j]];
+                let up_left =       vec!['X', e_m[i-1][j-1], e_m[i-2][j-2], e_m[i-3][j-3]];
+                let up_right =      vec!['X', e_m[i-1][j+1], e_m[i-2][j+2], e_m[i-3][j+3]];
+                let down_left =     vec!['X', e_m[i+1][j-1], e_m[i+2][j-2], e_m[i+3][j-3]];
+                let down_right =    vec!['X', e_m[i+1][j+1], e_m[i+2][j+2], e_m[i+3][j+3]];
+
+                let maybe_xmases = vec![left, right, up, down, up_left, up_right, down_left, down_right];
+
+                for maybe in maybe_xmases {
+                    let stringed: String = maybe.iter().collect();
+                    if stringed == xmas {
+                        xmas_count += 1
+                    }
+                }
             }
-            i += 1;
-            j += 1;
         }
     }
-    xmascount
+
+    xmas_count
 }
 
-fn transpose (v: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    assert!(!v.is_empty());
-    let len = v[0].len();
-    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
-    (0..len)
-        .map(|_| {
-            iters
-                .iter_mut()
-                .map(|n| n.next().unwrap())
-                .collect::<Vec<char>>()
-        })
-        .collect()
-}
-
-fn char_to_string_vec (char_vecs: Vec<Vec<char>>) -> Vec<String> {
-    let mut string_vec = Vec::new();
-    for cvec in char_vecs {
-        let to_string: String = cvec.iter().collect();
-        string_vec.push(to_string)
-    }
-    string_vec
-}
-
-fn diagonal_matrix (matrix: Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let len_y = matrix.len();
-    let len_x = matrix[0].len();
-    assert!(len_y == len_x);
+fn expand_matrix (matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut expanded: Vec<Vec<char>> = Vec::new();
+    let padding = vec!['A'; matrix.len()+6];
+    let triple_a = vec!['A', 'A', 'A'];
     
-    let mut diag_matr = Vec::new();
-    for h in 4..=len_y {
-        let mut low_vec = Vec::new();
-        let mut high_vec = Vec::new();
-        let mut i = len_y - h;
-        let mut j = 0;
-        while i != len_y {
-            let push_low = matrix[i][j];
-            let push_high = matrix[j][i];
-            low_vec.push(push_low);
-            high_vec.push(push_high);
-            i += 1;
-            j += 1;
-        }
-        diag_matr.push(low_vec);
-        diag_matr.push(high_vec);
-    }
-    let _last = diag_matr.pop();
+    for _ in 0..3 {expanded.push(padding.clone())};
 
-    diag_matr
+    for line in matrix {
+        let mut new_line: Vec<char> = Vec::new();
+        new_line.extend(&triple_a);
+        new_line.extend(line);
+        new_line.extend(&triple_a);
+        expanded.push(new_line);
+    }
+
+    for _ in 0..3 {expanded.push(padding.clone())};
+
+    expanded
 }
 
 fn count_xed_mases (matrix: &Vec<Vec<char>>) -> u32 {
