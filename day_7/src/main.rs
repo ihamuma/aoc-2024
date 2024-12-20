fda8e15f5bc37bfc5d4b95ead64761a7ca221926
ihamuma <39342539+ihamuma@users.noreply.github.com>
2024-12-20 23:10:11 +0200

    Day Seven Part One

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,99 +1,77 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-use std::time::Instant;
+use anyhow::{Context, Ok, Result};
 
-// Current solution 434063141838 is too low
-fn main() {
-    let path: &str = "./input.txt";
-    let by_line: Vec<String> = file_to_string_vec(path);
-    let sum_calculation_map = map_sums_and_calculations(by_line);
+fn main () {
+    let input = std::fs::read_to_string("./input.txt").unwrap();
 
-    let before = Instant::now();
+    let equations: Vec<Equation> = input.lines()
+                                        .map(|line| Equation::from(line).unwrap())
+                                        .collect();
 
-    let mut i = 0;
-    let mut valids_sum: u128 = 0;
-    let mut invalid_keys: Vec<u128> = Vec::new();
-    for (sum, calc) in sum_calculation_map {
-        i += 1;
-        println!("Evaluating {i} of 850");
-        let product = calc.iter().fold(1, |res, a| res * a);
-        if sum > product {
-            //println!("Bigger: {sum} > {product}")
-            invalid_keys.push(sum);
-            continue
-        }
-        if sum == product {
-            //println!("Equal: {sum} = {product}")
-            valids_sum = valids_sum.checked_add(sum).expect("Overall sum variable overflowed!");
-            continue
-        }
+    let result: u64 = equations.iter()
+                                .map(|eq| Equation::validate(eq).unwrap())
+                                .sum();
 
-        let all_options = evaluate_all(&calc);
-        if all_options.contains(&sum) {
-            valids_sum = valids_sum.checked_add(sum).expect("Overall sum variable overflowed!");
-        } else {
-            invalid_keys.push(sum);
-        }
-
-    }
-
-    println!("The total calibration result is {valids_sum}");
-    println!("There are {} invalid test values", invalid_keys.len());
-    println!("Elapsed time: {:.2?}", before.elapsed());
+    println!("Result: {}", result);
 }
 
-fn file_to_string_vec (path: &str) -> Vec<String> {
-    let file: File = File::open(&path).expect("Couldn't open file");
-    let reader: BufReader<File> = BufReader::new(file);
-
-    let mut by_line: Vec<String> = Vec::new();
-    for line in reader.lines() {
-        match line {
-            Ok(_) => by_line.push(line.unwrap()),
-            Err(_) => break,
-        }
-    }
-
-    by_line
+struct Equation {
+    result: u64,
+    numbers: Vec<u64>,
 }
 
-fn map_sums_and_calculations (mappable: Vec<String>) -> HashMap<u128, Vec<u128>> {
-    let mut map = HashMap::new();
-    for line in &mappable {
-        if let Some((sum, to_calculate)) = line.split_once(": ") {
-            let sum = sum.parse::<u128>().unwrap();
-            let calc_vec: Vec<u128> = to_calculate.split(" ")
-                                        .map(|x| x.parse::<u128>().unwrap())
-                                        .collect();            
-            // Technically unsafe, but previously checked for duplicates.
-            map.insert(sum, calc_vec);
-        }
-    }
-    map
-}
+static OPERATORS: [fn(u64, u64) -> u64; 2] = [
+    |a: u64, b: u64| -> u64 { a + b },
+    |a: u64, b:u64| -> u64 { a * b },
+];
 
-// This is dirty ChatGPT recursive code, made "safe" with overflow checks
-fn evaluate_all(nums: &Vec<u128>) -> HashSet<u128> {
-    let mut results = HashSet::new();
-
-    if nums.len() == 1 {
-        results.insert(nums[0]);
-        return results;
-    }
-
-    for i in 1..nums.len() {
-        let left = evaluate_all(&nums[0..i].to_vec());
-        let right = evaluate_all(&nums[i..].to_vec());
-
-        for &l in &left {
-            for &r in &right {
-                results.insert(l.checked_add(r).expect("{l} + {r} overflowed!"));
-                results.insert(l.checked_mul(r).expect("{l} * {r} overflowed!"));
-            }
+impl Equation {
+    fn new () -> Equation {
+        Equation {
+            result: 0,
+            numbers: Vec::new(),
         }
     }
 
-    results
+    fn from (line: &str) -> Result<Equation> {
+        let mut equation = Equation::new();
+
+        if !line.contains(":") {
+            return Err(anyhow::anyhow!("Invalid equation input"));
+        }
+
+        let (result, numbers) = match line.split_once(": ") {
+            Some((res, nums)) => (  res.parse::<u64>().unwrap(),
+                                                nums.split(" ")
+                                                .map(|x| x.parse::<u64>().unwrap())
+                                                .collect() ),
+            None => return Err(anyhow::anyhow!("Couldn't split valid input"))
+                                      .context("Splitting to result and numbers failed"),
+        };
+
+        equation.result = result;
+        equation.numbers = numbers;
+
+        Ok(equation)
+    }
+
+    fn validate (&self) -> Result<u64> {
+        match Equation::eval(self.result, self.numbers[0], self.numbers[1..].to_vec()).unwrap() {
+            true  => Ok(self.result),
+            false => Ok(0),
+        }
+    }
+
+    fn eval(expected_result: u64, acc: u64, numbers: Vec<u64>) -> Result<bool> {
+        if numbers.is_empty() {
+            return Ok(expected_result == acc);
+        }
+
+        Ok(
+            OPERATORS.iter().any(|op| {
+                Equation::eval(
+                    expected_result, op(acc, numbers[0]), numbers[1..].to_vec()
+                ).unwrap()
+            })
+        )
+    }
 }
