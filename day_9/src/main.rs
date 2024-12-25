07c3b776790639d1b26a5a99fe087072ace3a470
ihamuma <markus.ihamuotila@gmail.com>
2024-12-25 23:52:52 +0200

    Simplified input reading, added progress bar

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,12 +1,11 @@
+use indicatif::{ProgressBar, ProgressIterator};
 use std::fs;
 
 fn main() {
     let disk_map: Vec<u8> = fs::read_to_string("./input.txt").unwrap()
-                                                                    .chars()
-                                                                    .collect::<Vec<char>>()
-                                                                    .into_iter()
-                                                                    .map(|ch|ch.to_digit(10).unwrap() as u8)
-                                                                    .collect();
+                                                                .chars()
+                                                                .map(|ch|ch.to_digit(10).unwrap() as u8)
+                                                                .collect();
 
     let mut expanded_layout = Vec::new();
     let mut id: i16 = -1;
@@ -25,6 +24,9 @@ fn main() {
         }
     }
 
+    // Cloning to allow pt. 2 manipulations
+    let mut expanded_layout_two = expanded_layout.clone();
+
     let empty_count = expanded_layout.iter()
                                             .filter(|&n| *n == -1)
                                             .count();
@@ -41,12 +43,77 @@ fn main() {
         } else {
             compacted_layout.push(expanded_layout[i]);
         }
-    }
+    };
 
     let mut check_sum = 0;
     for (i, mem) in compacted_layout.into_iter().enumerate() {
         check_sum += i as i64 * mem as i64
     }
 
-    println!("The check sum is {check_sum}");
+    println!("The initial check sum is {check_sum}");
+
+    let mut i = expanded_layout_two.len() - 1;
+    //println!("{:?}", &expanded_layout_two[..9]);
+
+    let bar_size = (expanded_layout_two.len() - 2) as u64;
+    let bar = ProgressBar::new(bar_size);
+
+    // First part of memory is always occupied, no need to check last position
+    while i > 0 {
+        // Current element to examine
+        let cur_id = expanded_layout_two[i];
+
+        // While next id == previous id, iterate until changes
+        let mut j = i-1;
+        while expanded_layout_two[j] == cur_id && j > 0 {
+            bar.inc(1);
+            j -= 1;
+        }
+        // Add one so as to not take unmatching id
+        let lower_bound = j+1;
+        let memory_need = i-j;
+
+        // Slice layout at low bound to enable mutation
+        let (left, right) = expanded_layout_two.split_at_mut(lower_bound);
+
+        // Create slice based on lowest & highest 
+        let movable = &mut right[..memory_need];
+
+        let mut allocatable = false;
+        let mut lower_free = 0;
+        let mut upper_free = 0+memory_need;
+
+        if left.len() >= memory_need {
+            for k in 0..left.len()-memory_need {
+                let test_slice = &mut left[k..k+memory_need];
+                if test_slice.iter().all(|&x| x == -1) {
+                    lower_free = k;
+                    upper_free = k+memory_need;
+                    allocatable = true;
+                    break
+                }
+            }
+        }
+
+        if allocatable {
+            let allocated = &mut left[lower_free..upper_free];
+            allocated.swap_with_slice(movable);
+            //println!("{:?}", &expanded_layout_two[..19]);
+        }
+
+        i = j;
+    }
+
+    bar.finish();
+
+    let mut second_check_sum = 0;
+    for (i, mem) in expanded_layout_two.into_iter().enumerate().progress() {
+        if mem != -1 {
+            second_check_sum += i as i64 * mem as i64
+        }
+    }
+
+    println!("The final check sum is {second_check_sum}")
+    // Current too high 6390782022205
+
 }
